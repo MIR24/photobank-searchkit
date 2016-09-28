ef312d4bb583d2ff7eedc5a123d098534945a7cc
barantaran <yourchev@gmail.com>
2016-09-28 17:28:43 +0300

    Replace IP with domain name

--- src/app/src/ResultComponents.tsx
+++ src/app/src/ResultComponents.tsx
@@ -3,7 +3,7 @@ import * as _ from "lodash";
 
 export const MovieHitsGridItem = (props)=> {
   const {bemBlocks, result} = props
-  let url = "http://195.26.178.77/" + result._source.imdbId+ "/" + result._source.plot
+  let url = "https://photo.mir24.tv/" + result._source.imdbId+ "/" + result._source.plot
   const source:any = _.extend({}, result._source, result.highlight)
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
