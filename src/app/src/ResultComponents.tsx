d2687b69cb096228e99c6bf72dc24dba687f42c8
vanzi6 <ivanl487@gmail.com>
2017-02-16 18:06:41 +0300

    https://mir24tv.atlassian.net/browse/PHOT-185

--- src/app/src/ResultComponents.tsx
+++ src/app/src/ResultComponents.tsx
@@ -6,12 +6,18 @@ export const MovieHitsGridItem = (props)=> {
   let url = "https://photo.mir24.tv/" + result._source.imdbId+ "/" + result._source.plot
   const source:any = _.extend({}, result._source, result.highlight)
   let imgInfo = source.exifimagewidth + ' x ' + source.exifimagelength;
+  let posOfLastForwardSlash = result._source.poster.lastIndexOf( "/" );
+  let newImgUrl = result._source.poster.substring(0, posOfLastForwardSlash)+"/585x440_cropped.jpg";
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
       <a href={url} onClick={(e)=>{e.preventDefault(); console.log("195.26.178.77/plugins/imageviewer/site/direct.php?s="+result._source.imdbId)}}>
         <div >
-          <img data-qa="poster" className={bemBlocks.item("poster")} src={result._source.poster} width="240" height="240"/>
-          <div className="div-img-overlay" dangerouslySetInnerHTML={{__html:imgInfo}}></div>
+          <div className="container">
+            <div className="image-container">
+              <img data-qa="poster" className={bemBlocks.item("poster")} src={newImgUrl}/>
+              <p>{imgInfo}</p>
+            </div>
+          </div>
           <div data-qa="title" className="div-title" dangerouslySetInnerHTML={{__html:source.title}}>
           </div>
         </div>
@@ -24,10 +30,12 @@ export const MovieHitsListItem = (props)=> {
   const {bemBlocks, result} = props
   let url = "http://www.imdb.com/title/" + result._source.imdbId
   const source:any = _.extend({}, result._source, result.highlight)
+  let posOfLastForwardSlash = result._source.poster.lastIndexOf( "/" );
+  let newImgUrl = result._source.poster.substring(0, posOfLastForwardSlash)+"/585x440_cropped.jpg";
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
       <div className={bemBlocks.item("poster")}>
-        <img data-qa="poster" src={result._source.poster}/>
+        <img data-qa="poster" src={newImgUrl}/>
       </div>
       <div className={bemBlocks.item("details")}>
         <a href={url} target="_blank"><h2 className={bemBlocks.item("title")} dangerouslySetInnerHTML={{__html:source.title}}></h2></a>
