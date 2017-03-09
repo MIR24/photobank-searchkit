3333fae1f86d35dabd6bc7c6726ee2cb36ec5c9b
vanzi6 <ivanl487@gmail.com>
2017-03-09 12:44:52 +0300

    https://mir24tv.atlassian.net/browse/PHOT-186

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -79,6 +79,11 @@ export class App extends React.Component<any, any> {
               <RangeFilter min={0} max={100} field="metaScore" id="metascore" title="Metascore" showHistogram={true}/>
               <RangeFilter min={1000} max={3000} field="exifimagelength" id="exifimagelength" title="Высота изображения" showHistogram={true}/>
               <RangeFilter min={1000} max={3000} field="exifimagewidth" id="exifimagewidth" title="Ширина изображения" showHistogram={true}/>
+              <NumericRefinementListFilter id="alignment" title="Вертикальные/Горизонтальные" field="horizontal" options={[
+                {title:"Все"},
+                {title:"Вертикальные", from:0, to:1},
+                {title:"Горизонтальные", from:1, to:2}
+              ]}/>
               <RefinementListFilter operator="OR" id="author" title="Автор" field="author.raw" size={10}/>
       				<RefinementListFilter translations={{"facets.view_more":"View more writers"}} id="writers" title="Writers" field="writers.raw" operator="OR" size={10}/>
       				<RefinementListFilter id="countries" title="Countries" field="countries.raw" operator="OR" size={10}/>
